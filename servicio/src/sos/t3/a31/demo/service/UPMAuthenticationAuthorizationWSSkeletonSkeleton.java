79a9ff01d38445a96c04de7b3e535e863435a55b
ldelillo <ldelillo99@gmail.com>
2025-05-22 14:08:18 +0200

    Exist user??

--- servicio/src/sos/t3/a31/demo/service/UPMAuthenticationAuthorizationWSSkeletonSkeleton.java
+++ servicio/src/sos/t3/a31/demo/service/UPMAuthenticationAuthorizationWSSkeletonSkeleton.java
@@ -161,6 +161,7 @@ public class UPMAuthenticationAuthorizationWSSkeletonSkeleton {
                 caracterActual = 64;
             }
             res += (char) caracterActual;
+            
         }
         return res;
     }
