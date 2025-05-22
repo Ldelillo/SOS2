4cd0ec0185fb7a2611e0811e5cac71fb6578f41a
lsduser <lsduser@SOS-Restful.myguest.virtualbox.org>
2025-05-22 13:14:49 +0200

    Tests removeuser

--- servicio/src/sos/t3/a31/demo/service/tests.java
+++ servicio/src/sos/t3/a31/demo/service/tests.java
@@ -8,6 +8,8 @@ public class tests {
          
      UPMAuthenticationAuthorizationWSSkeletonSkeleton skeleton = new UPMAuthenticationAuthorizationWSSkeletonSkeleton();
 
+
+
         System.out.println("Testing addUser");
 
         es.upm.fi.sos.t3.backend.AddUser addUser = new es.upm.fi.sos.t3.backend.AddUser();
@@ -15,27 +17,29 @@ public class tests {
         addUser.getUser().setName("test1");
         es.upm.fi.sos.t3.backend.AddUserResponse response0 = skeleton.addUser(addUser);
 
-        System.out.println("Usuario test1 añadido (esperado = true):\t" + response0.get_return().getResult());
+        System.out.println("Usuario 'test1' añadido (esperado = true):\t" + response0.get_return().getResult());
+        System.out.println("Contraseña generada:\t" + response0.get_return().getPassword());
 
         response0 = skeleton.addUser(addUser);
 
-        System.out.println("Usuario test1 añadido de nuevo (esperado = false):\t" + response0.get_return().getResult());
+        System.out.println("Usuario 'test1' añadido de nuevo (esperado = false):\t" + response0.get_return().getResult());
 
         addUser.getUser().setName("test2");
         response0 = skeleton.addUser(addUser);
 
-        System.out.println("Usuario test2 añadido (esperado = true):\t" + response0.get_return().getResult());
+        System.out.println("Usuario 'test2' añadido (esperado = true):\t" + response0.get_return().getResult());
 
-        System.out.println(response0.get_return().getPassword());
+        System.out.println("Contraseña generada:\t" + response0.get_return().getPassword());
 
-        System.out.println("Testing ExistUser");
+
+
+
+        System.out.println("\nTesting ExistUser");
 
         es.upm.fi.sos.t3.backend.ExistUser existUser = new es.upm.fi.sos.t3.backend.ExistUser();
         existUser.setUsername(new es.upm.fi.sos.t3.backend.xsd.Username());
         existUser.getUsername().setName("test");
         es.upm.fi.sos.t3.backend.ExistUserResponse response1 = skeleton.existUser(existUser);
-
-        System.out.println("te jodes");
 
         System.out.println("Existe usuario 'test' (esperado = false):\t" + response1.get_return().getResult());
 
@@ -43,6 +47,25 @@ public class tests {
         response1 = skeleton.existUser(existUser);
 
         System.out.println("Existe usuario 'test1' (esperado = true):\t" + response1.get_return().getResult());
+
+
+
+
+
+        System.out.println("\nTesting removeUser");
+
+        es.upm.fi.sos.t3.backend.RemoveUser removeUser = new es.upm.fi.sos.t3.backend.RemoveUser();
+        removeUser.setRemoveUser(new es.upm.fi.sos.t3.backend.xsd.RemoveUser());
+        removeUser.getRemoveUser().setName("test1");
+        es.upm.fi.sos.t3.backend.RemoveUserResponse response2 = skeleton.removeUser(removeUser);
+
+        System.out.println("Usuario 'test1' eliminado (esperado = true):\t" + response2.get_return().getResult());
+
+        removeUser.getRemoveUser().setName("test");
+
+        System.out.println("Usuario 'test' eliminado (esperado = false):\t" + response2.get_return().getResult());
+
+
 
         }
 }
