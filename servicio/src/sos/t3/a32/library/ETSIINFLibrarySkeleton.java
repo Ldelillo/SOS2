fc3941ecc164fb1427b5ab9e3f6707f9428ac92c
ldelillo <ldelillo99@gmail.com>
2025-05-31 12:15:22 +0200

    Login y AddUser

--- servicio/src/sos/t3/a32/library/ETSIINFLibrarySkeleton.java
+++ servicio/src/sos/t3/a32/library/ETSIINFLibrarySkeleton.java
@@ -13,33 +13,37 @@ import java.util.List;
 import org.apache.axis2.AxisFault;
 
 import es.upm.etsiinf.sos.*;
-import es.upm.etsiinf.sos.model.xsd.User;
+import es.upm.etsiinf.sos.AddUserResponse;
+import es.upm.etsiinf.sos.auth.*;
+import es.upm.etsiinf.sos.auth.UPMAuthenticationAuthorizationWSSkeletonStub.*;
+import es.upm.etsiinf.sos.model.xsd.*;
+
 /**
  * ETSIINFLibrarySkeleton java skeleton for the axisService
  */
 public class ETSIINFLibrarySkeleton {
 
+    private static List<String> userslogged;
 
-private static List<String> userslogged;
+    private static User admin;
 
+    private boolean loged;
+    private String userNameLogged;
 
-private User admin;
+    /**
+     * Constructor
+     */
+    public ETSIINFLibrarySkeleton() {
+        loged = false;
+        if (admin.equals(null)) {
+            admin = new User();
+            admin.setName("admin");
+            admin.setPwd("admin");
+        }
+        if (userslogged.equals(null))
+            userslogged = new ArrayList<String>();
+    }
 
-private boolean loged;
-
-/**
- * Constructor 
- */
-public ETSIINFLibrarySkeleton(){
-loged = false;
-
-admin = new User();
-admin.setName("admin");
-admin.setPwd("admin");
-
-userslogged = new ArrayList<String>();
-
-}
     /**
      * Auto generated method signature
      * 
@@ -115,10 +119,37 @@ userslogged = new ArrayList<String>();
      * @return addUserResponse
      */
 
-    public es.upm.etsiinf.sos.AddUserResponse addUser(
-            es.upm.etsiinf.sos.AddUser addUser) {
-        // TODO : fill this with the necessary business logic
-        throw new java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#addUser");
+    public es.upm.etsiinf.sos.AddUserResponse addUser(es.upm.etsiinf.sos.AddUser addUser) {
+        AddUserResponse response = new AddUserResponse();
+        es.upm.etsiinf.sos.model.xsd.AddUserResponse response4 = new es.upm.etsiinf.sos.model.xsd.AddUserResponse();
+        if(userNameLogged.equals(admin.getName())){
+            try {
+                UPMAuthenticationAuthorizationWSSkeletonStub stub = new UPMAuthenticationAuthorizationWSSkeletonStub();
+
+                UPMAuthenticationAuthorizationWSSkeletonStub.AddUser addUser2 = new UPMAuthenticationAuthorizationWSSkeletonStub.AddUser(); 
+                UserBackEnd userAux = new UserBackEnd();
+                userAux.setName(addUser.getArgs0().getUsername());
+                addUser2.setUser(userAux);
+
+                UPMAuthenticationAuthorizationWSSkeletonStub.AddUserResponse response2 = stub.addUser(addUser2);
+                AddUserResponseBackEnd response3 = response2.get_return();
+
+                response4.setResponse(response3.getResult());
+                //No registro la contraseña si el usuario ya existe
+                if(response3.getResult())
+                    response4.setPwd(response3.getPassword());
+                response.set_return(response4);
+
+            } catch (Exception e) {
+                // TODO:ERRORES?
+                e.printStackTrace();
+            }
+        }
+        else{
+            response4.setResponse(false);
+            response.set_return(response4);
+        }
+        return response;
     }
 
     /**
@@ -170,26 +201,55 @@ userslogged = new ArrayList<String>();
      */
 
     public es.upm.etsiinf.sos.LoginResponse login(es.upm.etsiinf.sos.Login login) {
-        
-        // TODO : fill this with the necessary business logic
-        // throw new java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#login");
-    
-    es.upm.etsiinf.sos.LoginResponse response = new es.upm.etsiinf.sos.LoginResponse();
-    
-    try {
-        es.upm.etsiinf.sos.auth.UPMAuthenticationAuthorizationWSSkeletonStub stub = new UPMAuthenticationAuthorizationWSSkeletonStub();
-    
+        es.upm.etsiinf.sos.LoginResponse response = new es.upm.etsiinf.sos.LoginResponse();
+        // Si se llama a la funcion con el usuario ya iniciado da true
+        // independientemente de la entrada.
+        if (loged) {
+            Response entradaRep = new Response();
+            entradaRep.setResponse(userNameLogged.equals(login.getArgs0().getName()));
+            response.set_return(entradaRep);
+            return response;
+        }
+        // Entrada de admin
+        if (login.getArgs0().getName().equals(admin.getName())) {
+            Response entradaRep = new Response();
+            if (login.getArgs0().getPwd().equals(admin.getPwd())) {
+                userNameLogged = login.getArgs0().getName();
+                userslogged.add(userNameLogged);
+                entradaRep.setResponse(true);
+            } else {
+                entradaRep.setResponse(false);
+            }
+            response.set_return(entradaRep);
+            return response;
+        }
 
-        es.upm.etsiinf.sos.auth.UPMAuthenticationAuthorizationWSSkeletonStub.LoginResponse response2 = 
-            stub.login(new es.upm.etsiinf.sos.auth.UPMAuthenticationAuthorizationWSSkeletonStub.Login() );
-    
-    } catch (Exception e) {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
-    }
-    
-    return response;
-    
+        try {
+            UPMAuthenticationAuthorizationWSSkeletonStub stub = new UPMAuthenticationAuthorizationWSSkeletonStub();
+            UPMAuthenticationAuthorizationWSSkeletonStub.Login loginAux = 
+            new es.upm.etsiinf.sos.auth.UPMAuthenticationAuthorizationWSSkeletonStub.Login();
+            
+            LoginBackEnd loginAux2 = new LoginBackEnd();
+            loginAux2.setName(login.getArgs0().getName());
+            loginAux2.setPassword(login.getArgs0().getPwd());
+            loginAux.setLogin(loginAux2);
+
+            es.upm.etsiinf.sos.auth.UPMAuthenticationAuthorizationWSSkeletonStub.LoginResponse response2 = stub
+                    .login(loginAux);
+
+            LoginResponseBackEnd response3 = response2.get_return();
+            Response response4 = new Response();
+            response4.setResponse(response3.getResult());
+            response.set_return(response4);
+            userNameLogged = login.getArgs0().getName();
+            userslogged.add(userNameLogged);
+        } catch (Exception e) {
+            // TODO ERRORES?
+            e.printStackTrace();
+        }
+
+        return response;
+
     }
 
     /**
